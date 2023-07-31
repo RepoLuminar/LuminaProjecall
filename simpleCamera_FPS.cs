5d7278171c6f18bf2fa455a3f8ff3c50d040f57d
RepoLuminar <140871945+RepoLuminar@users.noreply.github.com>
2023-07-31 12:58:40 +0700

    Create simpleCamera_FPS.cs

---
+++ simpleCamera_FPS.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFPS : MonoBehaviour
+{
+     public float lookSpeed = 2f;
+    public float maxLookUpAngle = 80f;
+    public float maxLookDownAngle = 80f;
+
+    private float rotationX = 0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; // Lock the cursor to the center of the screen
+    }
+
+    private void Update()
+    {
+        // Camera Look
+        float lookHorizontal = Input.GetAxis("Mouse X") * lookSpeed;
+
+        transform.Rotate(Vector3.up * lookHorizontal);
+
+        // Limit vertical look rotation
+        float lookVertical = Input.GetAxis("Mouse Y") * lookSpeed;
+        rotationX -= lookVertical;
+        rotationX = Mathf.Clamp(rotationX, -maxLookDownAngle, maxLookUpAngle);
+
+        transform.localRotation = Quaternion.Euler(rotationX, 0f, 0f);
+    }
+}
